33aef438334f24c37a7f2f21815ec7d2e58bfe7c
Ayesh <ayeshbuddima@gmail.com>
2019-08-26 14:51:43 +0530

    1.0

---
+++ src/main/java/com/project/attendance/AttendanceUsingFingerprintScannerApplication.java
@@ -0,0 +1,13 @@
+package com.project.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceUsingFingerprintScannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceUsingFingerprintScannerApplication.class, args);
+    }
+
+}
