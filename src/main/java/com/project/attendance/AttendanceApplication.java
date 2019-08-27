225e276fd3d99a4f1f4c3f5ac24b4d2e23e29334
Ayesh <ayeshbuddima@gmail.com>
2019-08-27 15:34:07 +0530

    system 1.0

--- src/main/java/com/project/attendance/AttendanceApplication.java
+++ src/main/java/com/project/attendance/AttendanceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AttendanceUsingFingerprintScannerApplication {
+public class AttendanceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AttendanceUsingFingerprintScannerApplication.class, args);
+        SpringApplication.run(AttendanceApplication.class, args);
     }
 
 }
--- src/test/java/com/project/attendance/AttendanceApplicationTests.java
+++ src/test/java/com/project/attendance/AttendanceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AttendanceUsingFingerprintScannerApplicationTests {
+public class AttendanceApplicationTests {
 
     @Test
     public void contextLoads() {
