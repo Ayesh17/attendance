38fc6805088de80613d656f8eb992a9fd7f014f0
Ayesh <ayeshbuddima@gmail.com>
2019-08-28 10:41:27 +0530

    student

---
+++ src/main/java/com/project/attendance/dao/StudentDAO.java
@@ -0,0 +1,38 @@
+package com.project.attendance.dao;
+
+import com.project.attendance.model.Student;
+import com.project.attendance.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentDAO {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    //to save a student
+    public Student save(Student student){
+        return studentRepository.save(student);
+    }
+
+    //to search all students
+    public List<Student> findAll(){
+        return studentRepository.findAll();
+    }
+
+    //get a student by id
+    public Student findById(Long id){
+        return studentRepository.findById(id).orElse(null);
+    }
+
+
+    //delete a student
+    public void delete(Student student){
+        studentRepository.delete(student);
+    }
+
+
+}
