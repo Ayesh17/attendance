3da396c7f4e4e70f5bfbcae33d4ed21d146c9d3a
Ayesh <ayeshbuddima@gmail.com>
2019-08-29 13:08:49 +0530

    update

--- src/main/java/com/project/attendance/controller/StudentEnrollmentController.java
+++ src/main/java/com/project/attendance/controller/StudentEnrollmentController.java
@@ -5,7 +5,11 @@ import com.project.attendance.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -20,5 +24,27 @@ public class StudentEnrollmentController {
         List<Student> studentDetails=studentDAO.findAll();
         model.addAttribute("studentDetails",studentDetails);
         return "index";
+    }
+
+    @RequestMapping("/new")
+    public String addStudent(Model model){
+        Student student=new Student();
+        model.addAttribute("student",student);
+        return "registerStudent";
+    }
+
+    @RequestMapping(value="/save",method= RequestMethod.POST)
+    public String saveStudent(@ModelAttribute("student") Student student){
+        studentDAO.save(student);
+        return  "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView updateStudent(@PathVariable(name="id")Long id){
+        ModelAndView mav=new ModelAndView(("updateStudent"));
+
+        Student student=studentDAO.findById(id);
+        mav.addObject("student",student);
+        return  mav;
     }
 }
