0a48a9c1339279cca3dfb8e59c7180a80b523d49
Ayesh <ayeshbuddima@gmail.com>
2019-10-29 12:24:21 +0530

    students CSV

--- src/main/java/com/project/attendance/controller/StudentEnrollmentController.java
+++ src/main/java/com/project/attendance/controller/StudentEnrollmentController.java
@@ -3,16 +3,23 @@ package com.project.attendance.controller;
 import com.project.attendance.dao.CourseDAO;
 import com.project.attendance.dao.StudentDAO;
 import com.project.attendance.model.Course;
+import com.project.attendance.model.LectureHall;
 import com.project.attendance.model.Student;
+import com.project.attendance.util.LectureHallCSV;
+import com.project.attendance.util.StudentsCSV;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
@@ -66,5 +73,59 @@ public class StudentEnrollmentController {
     public String deleteProduct(@PathVariable(name="id") Long id){
         studentDAO.delete(id);
         return  "redirect:/student";
+    }
+
+    //Save the uploaded file to this folder
+    private static String UPLOADED_FOLDER = "F:\\upload\\";
+
+    @PostMapping("/student/upload") // //new annotation since 4.3
+    public String StudentHallCSVUpload(@RequestParam("file") MultipartFile file, HttpServletRequest request,
+                                       RedirectAttributes redirectAttributes) {
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:/student/csv";
+        }
+
+        try {
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+            request.setAttribute("path", path);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return "forward:/student/saveAll";
+    }
+
+
+
+    @RequestMapping(value="/student/saveAll",method= RequestMethod.POST)
+    public String saveAllStudents(@ModelAttribute("student") Student student, HttpServletRequest request,RedirectAttributes redirectAttributes){
+        Path path = (Path) request.getAttribute("path");
+        StudentsCSV csv=new StudentsCSV();
+        Path[] args={path} ;
+        List<Student> list = StudentsCSV.main(args);
+        try {
+            studentDAO.saveAll(list);
+        }catch (Exception e){
+            redirectAttributes.addFlashAttribute("message", "Duplicate Entry");
+            return  "redirect:/student";
+        }
+        return  "redirect:/student";
+    }
+
+    @RequestMapping("/student/csv")
+    public String view(Model model){
+        List<Student> studentDetails= studentDAO.findAll();
+        model.addAttribute("studentDetails",studentDetails);
+        return "addAllStudents";
     }
 }
